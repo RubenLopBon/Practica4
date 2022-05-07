511751337584cf7c69fc4893b1fa952f18da5745
Acroren <sms.rubenlopez@gmail.com>
2022-05-07 16:51:21 +0200

    ni idea

--- Practica4/Procesador.cs
+++ Practica4/Procesador.cs
@@ -10,8 +10,22 @@ namespace procesador
     {
         public int ContarPalabras(string texto)
         {
-            throw new Exception("Función no implementada"); 
-            //return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
         }
+
+        /* Funcionalidad 1,  red
+        public int ContarPalabras(string texto)
+        {
+            throw new Exception("Función no implementada"); 
+        }
+
+        Funcionalidad 1  green y Funcionalidad 2 red
+        public int ContarPalabras(string texto)
+        {
+            return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+         * 
+         * 
+         */
     }
 }
