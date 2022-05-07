3c342b1286b1b24b0deb43ce07ae955fcb74bb7e
LauraPiti23 <laura.cavero1@estudiant.uib.es>
2022-05-07 21:13:12 +0200

    Test's y TDD's restantes
    - Test Funcionalidad 3
    - Test Funcionalidad 4
    - TDD funcionalidad 2
    - TDD funcionalidad 3
    - TDD funcionalidad 4

--- Practica4/Procesador.cs
+++ Practica4/Procesador.cs
@@ -1,31 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace procesador
+﻿namespace procesador
 {
     public class Procesador
     {
         public int ContarPalabras(string texto)
         {
-            return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            if (texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length == 0)
+            {
+                throw new Exception("No existe ninguna palabra");
+            }
+
+            int num = 0, aux = 0;
+            string[] frases = texto.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < frases.Length; i++)
+            {
+                aux = frases[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+                if (aux > num) num = aux;
+            }
+
+            return num;
         }
 
-        /* Funcionalidad 1,  red
+        /* 
+        *********************************** Funcionalidad 1 RED ***********************************
         public int ContarPalabras(string texto)
         {
             throw new Exception("Función no implementada"); 
         }
 
-        Funcionalidad 1  green y Funcionalidad 2 red
+        *********************** Funcionalidad 1 GREEN | Funcionalidad 2 RED ***********************
         public int ContarPalabras(string texto)
         {
             return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
         }
-         * 
-         * 
+
+        ********************* Funcionalidad 1 y 2 GREEN | Funcionalidad 3 RED *********************
+        public int ContarPalabras(string texto)
+        {
+            if (texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length == 0) return 0;
+
+            int num = 0;
+            string[] frases = texto.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < frases.Length; i++)
+            {
+                num = frases[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+
+            return num;
+        }
+
+        ********************* Funcionalidad 1 y 3 GREEN | Funcionalidad 2 RED *********************
+        * Funcionalidad 2 RED: Cuando la última frase no es la más larga da error, se contradice
+        * con devolver la funcionalidad 1 de devolver el número de palabras de la última frase
+        * ¿Habrá que añadir una doble comprobación?
+        public int ContarPalabras(string texto)
+        {
+            if (texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length == 0) return 0;
+
+            int num = 0, aux = 0;
+            string[] frases = texto.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < frases.Length; i++)
+            {
+                aux = frases[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+                if (aux > num) num = aux;
+            }
+
+            return num;
+        }
+
+        ******************* Funcionalidad 3 y 4 GREEN | Funcionalidad 1 y 2 RED *******************
+        *Funcionalidad 1 RED: Existen pruebas de contenido vacío con resultado 0 en vez de
+        *saltar una excepción, se contradicen
+        *Funcionalidad 2 RED: MISMO ERROR ANTERIOR
+        public int ContarPalabras(string texto)
+        {
+            if (texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length == 0)
+            {
+                throw new Exception("No existe ninguna palabra");
+            }
+
+            int num = 0, aux = 0;
+            string[] frases = texto.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < frases.Length; i++)
+            {
+                aux = frases[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+                if (aux > num) num = aux;
+            }
+
+            return num;
+        } 
          */
     }
 }
--- ProcesadorTest/UnitTest1.cs
+++ ProcesadorTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using procesador;
+using System;
 
 namespace ProcesadorTest
 {
@@ -8,12 +9,12 @@ namespace ProcesadorTest
         [Theory]
         [InlineData("hola que tal", 3)]
         [InlineData("hola", 1)]
-        [InlineData(" ", 0)]
-        [InlineData("", 0)]
+        // [InlineData(" ", 0)]
+        // [InlineData("", 0)]
         [InlineData("1 } *** \n", 4)]
         public void ContarPalabras_Texto_Exito(string texto, int resultadoEsperado)
         {
-            //Arramge
+            //Arrange
             var sut = new Procesador();
 
             //Act
@@ -31,7 +32,7 @@ namespace ProcesadorTest
         [InlineData("Pepe juega mucho", 3)]
         public void ContarPalabras_Frase_Exito(string texto, int resultadoEsperado)
         {
-            //Arramge
+            //Arrange
             var sut = new Procesador();
 
             //Act
@@ -39,6 +40,39 @@ namespace ProcesadorTest
 
             //Assert
             Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        [Theory]
+        [InlineData("hola que tal.", 3)]
+        [InlineData("Pepe juega. Al lol.", 2)]
+        [InlineData(".", 0)]
+        [InlineData("Pepe juega mucho. Al lol", 3)]
+        [InlineData("Pepe juega mucho", 3)]
+        public void ContarPalabras_FraseLarga_Exito(string texto, int resultadoEsperado)
+        {
+            //Arrange
+            var sut = new Procesador();
+
+            //Act
+            int resultado = sut.ContarPalabras(texto);
+
+            //Assert
+            Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("")]
+        public void ContarPalabras_Excepción_Exito(string texto)
+        {
+            //Arrange
+            var sut = new Procesador();
+
+            //Act
+            var exception = Assert.Throws<Exception>(() => sut.ContarPalabras(texto));
+
+            //Assert
+            Assert.Equal("No existe ninguna palabra", exception.Message);
         }
     }
 }
